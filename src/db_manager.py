44bcfa15c81131d28fb902370d710a655566b528
casper <oksanaozturk@gmail.com>
2024-03-04 18:15:01 +0300

    Внесены изменения на основе замечаний Наставника
    Согласно лучшим практикам https://www.psycopg.org/docs/faq.html#best-practices нужно переиспользовать conn а curr создавать для каждого запроса.  Conn у тебя переиспользуется, но cur ты используешь везде один и тот же. Нужно  для каждого запроса создавать и закрывать cur, так как если cur не закрыт не все ресурсы освобождаются.

--- src/db_manager.py
+++ src/db_manager.py
@@ -14,18 +14,18 @@ class DBManager:
         self.connection = None
         self.cursor = None
 
-    def connect(self) -> None:
+    def connect(self):
         """Подключает к БД PostgreSQL"""
         try:
             self.connection = psycopg2.connect(dbname=self.database_name, **self.params)
-            self.cursor = self.connection.cursor()
+            # self.cursor = self.connection.cursor()
             print("Успешное подключение к базе данных")
         except psycopg2.Error:
             print(f"Ошибка подключения к базе данных: {self.database_name}")
 
     def disconnect(self) -> None:
         """Отключает от БД PostgreSQL"""
-        self.cursor.close()
+        # self.cursor.close()
         self.connection.close()
         print("Отключение от базы данных")
 
@@ -33,6 +33,7 @@ class DBManager:
         """
         Метод, который получает список всех компаний и количество вакансий у каждой компании.
         """
+        self.cursor = self.connection.cursor()
         self.cursor.execute("""SELECT employer_name, COUNT(*) AS count_vacancies
         FROM vacancies
         INNER JOIN employers USING(employer_id)
@@ -41,35 +42,40 @@ class DBManager:
         rows = self.cursor.fetchall()
         for row in rows:
             print(*row)
+        self.cursor.close()
 
     def get_all_vacancies(self):
         """
         Метод, который, получает список всех вакансий с указанием названия компании,
         названия вакансии, зарплаты и ссылки на вакансию.
         """
+        self.cursor = self.connection.cursor()
         self.cursor.execute("""SELECT employer_name, vacancy_title, salary_from, salary_to, vacancy_url
         FROM vacancies
         INNER JOIN employers USING(employer_id)""")
         rows = self.cursor.fetchall()
         for row in rows:
             print(*row)
+        self.cursor.close()
 
     def get_avg_salary(self):
         """
         Метод, который получает среднюю зарплату по вакансиям
         """
-
+        self.cursor = self.connection.cursor()
         self.cursor.execute("""SELECT AVG(salary_from)
             FROM vacancies""")
         result = self.cursor.fetchone()
         result_1 = int(result[0])
         print(f"Cредняя зарплата по всем вакансиям - {result_1} руб.")
+        self.cursor.close()
 
     def get_vacancies_with_higher_salary(self):
         """
         Метод, который получает список всех вакансий,
         у которых зарплата выше средней по всем вакансиям.
         """
+        self.cursor = self.connection.cursor()
         self.cursor.execute("""SELECT employer_name, vacancy_title, city, salary_from, salary_to, vacancy_url 
         FROM vacancies
         INNER JOIN employers USING(employer_id)
@@ -78,12 +84,14 @@ class DBManager:
         rows = self.cursor.fetchall()
         for row in rows:
             print(*row)
+        self.cursor.close()
 
     def get_vacancies_with_keyword(self, keyword: str):
         """
         Метод, который получает список всех вакансий,
         в названии которых содержатся переданные в метод слова, например python.
         """
+        self.cursor = self.connection.cursor()
         self.cursor.execute(f"""SELECT employer_name, vacancy_title, city, vacancy_url, salary_from, salary_to 
         FROM vacancies
         INNER JOIN employers USING(employer_id)
@@ -93,3 +101,4 @@ class DBManager:
             print("К сожалению, по введенному Вами слову вакансий найдено не было, попробуйте ещё раз.")
         for row in rows:
             print(*row)
+        self.cursor.close()
